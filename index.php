9db78981ab0ab4e8b9a1aebf9e7c03102f4947a4
wilmeru64 <109971678+wilmeru64@users.noreply.github.com>
2022-07-25 10:33:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b>AmdocsNA Session WILMER - TEST</b></p>
+		<p><b>AmdocsNA Session WILMER - TEST- WITH WEBHOOK</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information </h2> 
+	<h2>PHP Information for WEBHOOK</h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
 </html>
